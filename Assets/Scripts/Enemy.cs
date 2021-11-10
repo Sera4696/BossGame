bcce0bbd73db75afecb3e0c95436b2d1acf51f36
Sera4696 <k019g1549@gmail.com>
2021-11-11 08:48:03 +0900

    弾のランダム生成

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private int reverse;
+    public float x;
+    public float y;
+    public float z;
+    // Start is called before the first frame update
+    void Start()
+    {
+        reverse = 1;
+
+        x = Random.Range(-0.5f, 0.5f);
+        y = Random.Range(-0.5f, 0.5f);
+        z = Random.Range(-0.5f, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        transform.position += new Vector3(x * reverse, y * reverse, z * reverse);
+
+        if (transform.position.x < -25 || transform.position.x > 25 ||
+            transform.position.y <  -8 || transform.position.y >  8 ||
+            transform.position.z < -25 || transform.position.z > 25)
+        {
+            reverse *= -1;
+           
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyIns.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyIns : MonoBehaviour
+{
+    [SerializeField] public GameObject S;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                // rangeAとrangeBのx座標の範囲内でランダムな数値を作成
+                float x = Random.Range(-25, 25);
+                // rangeAとrangeBのy座標の範囲内でランダムな数値を作成
+                float y = Random.Range(-8, 8);
+                // rangeAとrangeBのz座標の範囲内でランダムな数値を作成
+                float z = Random.Range(-25, 25);
+
+                Instantiate(S, new Vector3(x, y, z), Quaternion.identity);
+            }
+        }
+    }
+}
