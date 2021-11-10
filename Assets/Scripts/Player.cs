508a5bc73286c705048f7d06561175917df0f277
Sera4696 <k019g1549@gmail.com>
2021-11-11 01:22:01 +0900

    移動とタックル修正完了

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,62 +4,90 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float speed;                //オブジェクトのスピード
+    //プレイヤーの回転移動
+    [SerializeField] private int speed;                //オブジェクトのスピード
     [SerializeField] private int radius;               //円を描く半径
-    private Vector3 defPosition;      //defPositionをVector3で定義する。
-    float x;
-    float z;
-    float aroundTime;
-
-    [SerializeField] private Vector3 center;
-    [SerializeField] private Vector3 centerUp;
-    [SerializeField] private Vector3 centerDown;
-    
-
-    // 回転軸
-    [SerializeField] private Vector3 axis;
-
-    // 円運動周期
-    [SerializeField] private float _period;
-
+    [SerializeField] private Vector3 defPosition;      //defPositionをVector3で定義する。
+    [SerializeField] float x;
+    [SerializeField] float z;
+    [SerializeField] float aroundTime;
     [SerializeField] private bool isMove;
-    [SerializeField] private bool isMoveUpDown;
-    [SerializeField] private float moveCount;
 
-    [SerializeField] private int insCount;
-    [SerializeField] private int pointCount;
-
+    //プレイヤーのダッシュ
+　　[SerializeField] private bool isDash;
+    [SerializeField] private Transform dashPosition;
+    [SerializeField] private Vector3 dashNowPosition;
     [SerializeField] private float dashSpeedCount;
+    [SerializeField] private float dashSpeed;
+    
+    //その他
+    [SerializeField] public GameObject targetObject;
+    [SerializeField] private int reverse;
 
-    [SerializeField] public GameObject insPoint;
+    //[SerializeField] private float speed;                //オブジェクトのスピード
+    //[SerializeField] private int radius;               //円を描く半径
+    //private Vector3 defPosition;      //defPositionをVector3で定義する。
+    //float x;
+    //float z;
 
-    [SerializeField] private bool isDash;
 
-    [SerializeField] private GameObject[] points;
+    //[SerializeField] private Vector3 center;
+    //[SerializeField] private Vector3 centerUp;
+    //[SerializeField] private Vector3 centerDown;
 
-    [SerializeField] private LineRenderer lineRenderer;
+    //// 回転軸
+    //[SerializeField] private Vector3 axis;
+
+    //// 円運動周期
+    //[SerializeField] private float _period;
+
+
+    //[SerializeField] private bool isMoveUpDown;
+    //[SerializeField] private float moveCount;
+
+    //[SerializeField] private int insCount;
+    //[SerializeField] private int pointCount;
+
+
+
+    //[SerializeField] public GameObject insPoint;
+
+
+
+    //[SerializeField] private GameObject[] points;
+
+    //[SerializeField] private LineRenderer lineRenderer;
+
 
     //[SerializeField] private GameObject[] points;
 
     // Start is called before the first frame update
     void Start()
     {
-        speed = 0.8f;       
+          
+        //center = new Vector3(0, 10, 0);
+        //centerUp = new Vector3(0, 20, 0);
+        //centerDown = new Vector3(0, -20, 0);
+        //axis = new Vector3(0, 1, 0);
+
+        //isMoveUpDown = false;
+
+        //insCount = 3;
+        //pointCount = 0;
+        
+        //プレイヤーの回転移動
+        speed = 3;
+        radius = 30;
         aroundTime = 0;
+        defPosition = new Vector3(0, 10, 0);    //defPositionを自分のいる位置に設定する。
 
-        center = new Vector3(0, 10, 0);
-        centerUp = new Vector3(0, 20, 0);
-        centerDown = new Vector3(0, -20, 0);
-        axis = new Vector3(0, 1, 0);
-
-        isMoveUpDown = false;
-
-        insCount = 3;
-        pointCount = 0;
+        //プレイヤーのダッシュ
         isMove = true;
         isDash = false;
+        dashSpeed = 0.03f;
 
-        //defPosition = new Vector3(0,5,0);    //defPositionを自分のいる位置に設定する。
+        //その他
+        reverse = -1;
     }
 
     // Update is called once per frame
@@ -70,175 +98,209 @@ public class Player : MonoBehaviour
         MoveUpDown();
         Dash();
         Line();
-        //x = radius * Mathf.Sin(aroundTime * speed);      //X軸の設定
-        //z = radius * Mathf.Cos(aroundTime * speed);      //Z軸の設定
+        Look();
 
-        //transform.position = new Vector3(x + defPosition.x, defPosition.y, z + defPosition.z);  //自分のいる位置から座標を動かす。
+        //transform.LookAt(targetObject.transform);
     }
 
     public void Move()
     {
-        if(isMove)
+        
+        if (isMove)
         {
             if (Input.GetKey(KeyCode.A))
             {
-                aroundTime = 0.01f;
+                aroundTime += 0.01f;
             }
 
             else if (Input.GetKey(KeyCode.D))
             {
-                aroundTime = -0.01f;
+                aroundTime += -0.01f;
             }
 
-            else
-            {
-                aroundTime = 0;
-            }
+            x = radius * Mathf.Sin(aroundTime * speed) * reverse;      //X軸の設定
+            z = radius * Mathf.Cos(aroundTime * speed) * reverse;      //Z軸の設定  
 
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                isMoveUpDown = true;
-                Ins();
-            }
-
-            transform.RotateAround(center, axis, 360 / _period * aroundTime);
+            transform.position = new Vector3(x + defPosition.x, defPosition.y, z + defPosition.z);  //自分のいる位置から座標を動かす。
         }
+
+        //    else
+        //    {
+        //        aroundTime = 0;
+        //    }
+
+        //    if (Input.GetKeyDown(KeyCode.Space))
+        //    {
+        //        isMoveUpDown = true;
+        //        Ins();
+        //    }
+
+        //    transform.RotateAround(center, axis, 360 / _period * aroundTime);
+        //    //
+        //}
         //transform.RotateAround(center, axis, 360 / _period * aroundTime);
     }
 
-    public void MoveUpDown()
-    {
-        if (center.y == 10)
-        {
-            if (isMoveUpDown == true)
-            {
-                moveCount += 0.001f;
-                transform.position = Vector3.Lerp(transform.position, transform.position + centerDown, moveCount);
-
-                if (transform.position.y <= -10)
-                {
-                    transform.position = new Vector3(transform.position.x, -10, transform.position.z);
-                    center = new Vector3(0, -10, 0);
-
-                    isMoveUpDown = false;
-                    moveCount = 0;
-                }
-            }
-        }
-
-        if (center.y == -10)
-        {
-            if (isMoveUpDown == true)
-            {
-                moveCount += 0.001f;
-                transform.position = Vector3.Lerp(transform.position, transform.position + centerUp, moveCount);
-
-                if (transform.position.y >= 10)
-                {
-                    transform.position = new Vector3(transform.position.x, 10, transform.position.z);
-                    center = new Vector3(0, 10, 0);
-                    isMoveUpDown = false;
-                    moveCount = 0;
-                }
-            }
-        }
-    }
+    
 
     void Ins()
     {
-        if(insCount > 0)
-        {
-            points[pointCount] = Instantiate(insPoint, transform.position, Quaternion.identity);           
-            insCount--;
-            pointCount++;
-            if (pointCount > 3)
-                pointCount = 3;
-        }
+        //if(insCount > 0)
+        //{
+        //    points[pointCount] = Instantiate(insPoint, transform.position, Quaternion.identity);           
+        //    insCount--;
+        //    pointCount++;
+        //    if (pointCount > 3)
+        //        pointCount = 3;
+        //}
     }
 
     void Dash()
     {
-        if(Input.GetKeyDown(KeyCode.B) && points[0] != null)
+        if (Input.GetKeyDown(KeyCode.B))// && points[0] != null)
         {
+            dashNowPosition = dashPosition.position;
             isDash = true;
             isMove = false;
         }
 
-        if(isDash)
+        if (isDash == true)
         {
-            dashSpeedCount += 0.003f;
-            
-            if(transform.transform.position == points[pointCount - 1].transform.position && pointCount != 0)
+            dashSpeedCount += dashSpeed;
+            transform.position = Vector3.Lerp(transform.position, dashNowPosition, dashSpeedCount);
+
+            if(transform.position == dashNowPosition)
             {
-                Destroy(points[pointCount - 1]);
-                pointCount--;
-            }
+                reverse *= -1;
+                defPosition.y *= -1;
 
-            transform.position = Vector3.Lerp(transform.position, points[pointCount - 1].transform.position, dashSpeedCount);
-
-            if (transform.position == points[0].transform.position)
-            {
-                if(transform.position.y == 10)
-                {
-                    center.y = 10;
-                }
-
-                else if(transform.position.y == -10)
-                {
-                    center.y = -10;
-                }
-                pointCount = 0;
-                dashSpeedCount = 0;
-                insCount = 3;
                 isDash = false;
-                Destroy(points[0]);
                 isMove = true;
+                dashSpeedCount = 0;
             }
         }
+
+        //if(isDash)
+        //{
+        //    
+
+        //    if(transform.transform.position == points[pointCount - 1].transform.position && pointCount != 0)
+        //    {
+        //        Destroy(points[pointCount - 1]);
+        //        pointCount--;
+        //    }
+
+        //    
+
+        //    if (transform.position == points[0].transform.position)
+        //    {
+        //        if(transform.position.y >= 9.5f)
+        //        {                    
+        //            center.y = 10;
+        //        }
+
+        //        else if(transform.position.y == -10)
+        //        {
+        //            center.y = -10;
+        //        }
+        //        pointCount = 0;
+        //        dashSpeedCount = 0;
+        //        insCount = 3;
+        //        isDash = false;
+        //        Destroy(points[0]);
+        //        //反対にうつる処理
+        //        isMove = true;
+        //    }
+        //}
     }
 
     void Line()
     {
-        if (points[0] == null && points[1] == null && points[2] == null)
-        {
-            lineRenderer.enabled = false;
-        }
+    //    if (points[0] == null && points[1] == null && points[2] == null)
+    //    {
+    //        lineRenderer.enabled = false;
+    //    }
 
-        if (points[0] != null && points[1] == null && points[2] == null)
-        {
-            lineRenderer.enabled = true;
-            var positions = new Vector3[]{
-            points[0].transform.position,
-            transform.position};
+    //    if (points[0] != null && points[1] == null && points[2] == null)
+    //    {
+    //        lineRenderer.enabled = true;
+    //        var positions = new Vector3[]{
+    //        points[0].transform.position,
+    //        transform.position};
 
-            lineRenderer.positionCount = positions.Length;
+    //        lineRenderer.positionCount = positions.Length;
 
-            lineRenderer.SetPositions(positions);
-        }
+    //        lineRenderer.SetPositions(positions);
+    //    }
 
-        if (points[0] != null && points[1] != null && points[2] == null)
-        {
-            var positions = new Vector3[]{
-            points[0].transform.position,
-            points[1].transform.position,
-            transform.position};
+    //    if (points[0] != null && points[1] != null && points[2] == null)
+    //    {
+    //        var positions = new Vector3[]{
+    //        points[0].transform.position,
+    //        points[1].transform.position,
+    //        transform.position};
 
-            lineRenderer.positionCount = positions.Length;
+    //        lineRenderer.positionCount = positions.Length;
 
-            lineRenderer.SetPositions(positions);
-        }
+    //        lineRenderer.SetPositions(positions);
+    //    }
 
-        if (points[0] != null && points[1] != null && points[2] != null)
-        {
-            var positions = new Vector3[]{
-            points[0].transform.position,
-            points[1].transform.position,
-            points[2].transform.position,
-            transform.position};
+    //    if (points[0] != null && points[1] != null && points[2] != null)
+    //    {
+    //        var positions = new Vector3[]{
+    //        points[0].transform.position,
+    //        points[1].transform.position,
+    //        points[2].transform.position,
+    //        transform.position};
 
-            lineRenderer.positionCount = positions.Length;
+    //        lineRenderer.positionCount = positions.Length;
 
-            lineRenderer.SetPositions(positions);
-        }
+    //        lineRenderer.SetPositions(positions);
+    //    }
+    }
+
+    void Look()
+    {
+        var aim = targetObject.transform.position - transform.position;
+        var look = Quaternion.LookRotation(aim);
+        transform.localRotation = look;
+    }
+
+    public void MoveUpDown()
+    {
+        //if (center.y == 10)
+        //{
+        //    if (isMoveUpDown == true)
+        //    {
+        //        moveCount += 0.001f;
+        //        transform.position = Vector3.Lerp(transform.position, transform.position + centerDown, moveCount);
+
+        //        if (transform.position.y <= -10)
+        //        {
+        //            transform.position = new Vector3(transform.position.x, -10, transform.position.z);
+        //            center = new Vector3(0, -10, 0);
+
+        //            isMoveUpDown = false;
+        //            moveCount = 0;
+        //        }
+        //    }
+        //}
+
+        //if (center.y == -10)
+        //{
+        //    if (isMoveUpDown == true)
+        //    {
+        //        moveCount += 0.001f;
+        //        transform.position = Vector3.Lerp(transform.position, transform.position + centerUp, moveCount);
+
+        //        if (transform.position.y >= 10)
+        //        {
+        //            transform.position = new Vector3(transform.position.x, 10, transform.position.z);
+        //            center = new Vector3(0, 10, 0);
+        //            isMoveUpDown = false;
+        //            moveCount = 0;
+        //        }
+        //    }
+        //}
     }
 }
