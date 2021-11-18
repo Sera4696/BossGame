b2508c386abdd0ee16d39a48a4eb5a2944afb782
President <noza.burning@gmail.com>
2021-11-18 11:32:09 +0900

    カメラの揺れを実装

---
+++ Assets/Gamemanager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamemanager : MonoBehaviour
+{
+    //GameManagerはメインカメラにアタッチするスクリプトです
+    [SerializeField] private CameraShake CameraShake;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CameraShake = GetComponent<CameraShake>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            //画面の揺れ_左が時間_右揺れの強さ
+           StartCoroutine(CameraShake.Shake(0.2f, 1f));
+        }
+    }
+}
---
+++ Assets/Scripts/CameraShake.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    public IEnumerator Shake(float duration, float magnitude)
+    {
+        Debug.Log("ゆれた");
+        Vector3 originalPosition = transform.position;
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            transform.position = originalPosition + Random.insideUnitSphere * magnitude;
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = originalPosition;
+    }
+}
