0e39561a09f88c3a0faaee55ee037cbdedba45f3
dontStop810 <noza.burning@gmail.com>
2021-11-24 04:28:23 +0900

    ダメージエフェクトと貝殻を作りました

---
+++ Assets/ShellMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ShellMove : MonoBehaviour
+{
+
+    [SerializeField] private GameObject shellTop;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shellTop.transform.DORotate(new Vector3(90, 0, 0), 3);
+    }
+}
