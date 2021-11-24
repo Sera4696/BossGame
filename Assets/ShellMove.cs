a36664924e27d43815c21e2741eb384e7656c7b8
dontStop810 <noza.burning@gmail.com>
2021-11-24 20:49:43 +0900

    地形を作りました

--- Assets/ShellMove.cs
+++ Assets/ShellMove.cs
@@ -7,15 +7,41 @@ public class ShellMove : MonoBehaviour
 {
 
     [SerializeField] private GameObject shellTop;
+    private bool isOpen = true;
+    private float stopTimer = 0,limitter;
     // Start is called before the first frame update
     void Start()
     {
-        
+        limitter = Random.Range(5, 7);
     }
 
     // Update is called once per frame
     void Update()
     {
-        shellTop.transform.DORotate(new Vector3(90, 0, 0), 3);
+        if (isOpen == true)
+        {
+            shellTop.transform.DOLocalRotate(new Vector3(90, 0, 0), 5);
+            stopTimer += Time.deltaTime;
+            
+            if (stopTimer >= limitter)
+            {
+                isOpen = false;
+                stopTimer = 0;
+                limitter = Random.Range(5, 10);
+            }
+        }
+
+        if (isOpen == false)
+        {
+            shellTop.transform.DOLocalRotate(new Vector3(0, 0, 0), 5);
+            stopTimer += Time.deltaTime;
+            //limitter = Random.Range(5, 7);
+            if (stopTimer >= limitter)
+            {
+                isOpen = true;
+                stopTimer = 0;
+                limitter = Random.Range(5, 10);
+            }
+        }
     }
 }
